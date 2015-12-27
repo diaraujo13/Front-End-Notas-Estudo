e9b9d1a1d53b482b11f1d542d68647a7df133611
Izaias di Araújo <izaiasousa@gmail.com>
2015-12-27 20:14:31 -0200

    Create Childs.js

---
+++ firebase/Childs.js
@@ -0,0 +1,7 @@
+// Com o método push estaremos criando novos 'child' no link a qual
+// fazemos referência
+ref.push(object)
+
+// Para acessar um 'child' específico, utilizamos o .child(item_key)
+// pois todos 'child' possuem um id ao serem criados (por exemplo, criados via push)
+// - Com referncia ao child podemos passar o método .update()
