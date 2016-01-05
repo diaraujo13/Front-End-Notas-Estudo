b69d7c22369451939051982ffda0e125d2b9b8f8
Izaias di Araújo <izaiasousa@gmail.com>
2016-01-05 15:33:03 -0300

    Update lendo_dados_do_firebase.js

--- firebase/lendo_dados_do_firebase.js
+++ firebase/lendo_dados_do_firebase.js
@@ -19,7 +19,65 @@
         |--- Para acessar as propriedades de cada 'child', utilize .val() para pegar uma referência
         |--- ao objeto.
         • "child_removed" = toda vez que for removido um 'child'
-        -  "child_changed" = """
-
-
+        • "child_changed" = cada vez que 'child' sofrer um update
     */
+
+var ref = new Firebase("https://docs-examples.firebaseio.com/web/saving-data/fireblog/posts");
+var count = 0;
+ref.on("child_added", function(snap) {
+  count++;
+  console.log("added", snap.key());
+});
+ref.once("value", function(snap) {
+  console.log("initial data loaded!", Object.keys(snap.val()).length === count);
+});
+
+
+// Query
+// Com Firebase Database Queries nós podemos retornar somente dados que queremos com base
+// em alguns fatores:
+// |- Como desejamos ordenar nossos resultados?
+// |-- orderByChild     ()
+// |-- orderByKey       ()
+// |-- orderByValue     ()
+// |-- orderByPriority  ()
+// |- Podemos combinar com isso ainda mais outros métodos de "organização do resultado":
+// |-- limitToFirst     ()
+// |-- limitToLast      ()
+// |-- startAt          ()
+// |-- endAt            ()
+// |-- equalTo          ()
+
+{
+  "lambeosaurus": {
+    "dimensions": {
+      "height" : 2.1,
+      "length" : 12.5,
+      "weight": 5000
+    }
+  },
+  "stegosaurus": {
+    "dimensions": {
+      "height" : 4,
+      "length" : 9,
+      "weight" : 2500
+    }
+  }
+}
+
+var ref = new Firebase("https://dinosaur-facts.firebaseio.com/dinosaurs");
+// Ordenado como keys em comum, no caso de key aninhada utilizamos um caminho até a key,
+// ao invés de simplemente o nome da key
+ref.orderByChild("dimensions/height").on("child_added", function(snapshot) {
+  console.log(snapshot.key() + " was " + snapshot.val().height + " meters tall");
+});
+// OUTPUT:
+/*
+    linhenykus was 0.6 meters tall
+    pterodactyl was 0.6 meters tall
+    lambeosaurus was 2.1 meters tall
+    triceratops was 3 meters tall
+    stegosaurus was 4 meters tall
+    bruhathkayosaurus was 25 meters tall
+*/
+
