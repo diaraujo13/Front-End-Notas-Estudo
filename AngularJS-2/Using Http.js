6fe08f5fccaf176b82ec25e625b96462862f7643
Izaias di Araújo <izaiasousa@gmail.com>
2016-02-22 12:50:03 -0300

    Create Using Http.js

---
+++ AngularJS-2/Using Http.js
@@ -0,0 +1,8 @@
+// Http retorna um objeto do tipo Observable
+// ASSUNTOS:
+// - Como fazer requisições com Http
+// - Como tratar a resposta dessas requisições
+// - Operações de GET e POST
+// - Autenticação
+
+//
